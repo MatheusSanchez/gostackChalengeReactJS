834aa6c0911b7d35d895bf96d753ab251ab34b7e
Matheus Sanchez <matheus_sanchez@usp.br>
2020-06-19 11:10:46 -0300

    finish reactJs chalenge :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
-
+import React,{useState, useEffect} from "react";
 import "./styles.css";
+import api from './services/api'
 
 function App() {
+
+  const [projects,setProjects] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(reponse => {
+      setProjects(reponse.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+
+    api.post('/repositories',{
+      title: Date.now(),
+      url: Date.now(),
+    }).then(response => {
+      setProjects([...projects,response.data]);
+    });
   }
 
+
+
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then( () =>{
+      const newRespositories = projects.filter(project => project.id !== id);
+      setProjects(newRespositories);
+    }
+    )
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        
+          {
+            projects.map(project => (
+              <li key = {project.id}> {project.title} 
+                <button onClick={() => handleRemoveRepository(project.id)}>
+                  Remover
+               </button>
+              </li>
+            ))
+          }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
